865458e159177a3145e746378a2713fc5e22e8f0
DESKTOP-LTFLO15\desarrollo2 <carneiroluis2@gmail.com>
2022-01-13 14:53:20 -0400

    Heroes app

--- src/HookApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const HookApp = () => {
-    return (
-        <div>
-            <h1>HolaMundo</h1>
-        </div>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { EjemploRealRef } from './components/04-useRef/EjemploRealRef';
-
-
-
-// import { FocusScreen } from './components/04-useRef/FocusScreen';
-// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// import { FormWithCustomHooks } from './components/02-useEffect/FormWithCustomHooks';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { CounterApp } from './components/01-useState/CounterApp';
-// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// import { HookApp } from './HookApp';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <EjemploRealRef />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
